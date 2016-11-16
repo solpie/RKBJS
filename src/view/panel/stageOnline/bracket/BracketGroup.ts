615949d2011d93c64cc4612624a05675e70f0951
solpie <solpie.net@gmail.com>
2016-11-16 14:05:15 +0800

    clean

--- src/view/panel/stageOnline/bracket/BracketGroup.ts
+++ src/view/panel/stageOnline/bracket/BracketGroup.ts
@@ -1,5 +1,5 @@
 // 18452736
-function _makeGroup(parameters) {
+function _mkGroup(parameters) {
     let x = parameters.x;
     let y = parameters.y;
     let hints = parameters.hints ? parameters.hints : ['', ''];
@@ -15,20 +15,20 @@ function _makeGroup(parameters) {
     }
 }
 export let groupPosMap = {
-    "1": _makeGroup({x: 255, y: 95, hints: ['1号种子', "8号种子"]}),
-    "2": _makeGroup({x: 255, y: 95 + 145, hints: ['4号种子', "5号种子"]}),
-    "3": _makeGroup({x: 255, y: 95 + 145 * 2, hints: ['2号种子', "7号种子"]}),
-    "4": _makeGroup({x: 255, y: 95 + 145 * 3, hints: ['3号种子', "6号种子"]}),
-    "5": _makeGroup({x: 255, y: 805, hints: ['第1场败者', "第2场败者"]}),
-    "6": _makeGroup({x: 255, y: 805 + 145, hints: ['第3场败者', "第4场败者"]}),
-    "7": _makeGroup({x: 655, y: 168}),
-    "8": _makeGroup({x: 655, y: 457}),
-    "9": _makeGroup({x: 655, y: 900, hints: ['第7场败者', ""]}),
-    "10": _makeGroup({x: 655, y: 755, hints: ['第8场败者', ""]}),
-    "11": _makeGroup({x: 1055, y: 312}),
-    "12": _makeGroup({x: 1055, y: 825}),
-    "13": _makeGroup({x: 1460, y: 755, hints: ['第11场败者', ""]}),
-    "14": _makeGroup({x: 1460, y: 390, hints: ['', "第13场胜者"]}),
+    "1": _mkGroup({x: 255, y: 95, hints: ['1号种子', "8号种子"]}),
+    "2": _mkGroup({x: 255, y: 95 + 145, hints: ['4号种子', "5号种子"]}),
+    "3": _mkGroup({x: 255, y: 95 + 145 * 2, hints: ['2号种子', "7号种子"]}),
+    "4": _mkGroup({x: 255, y: 95 + 145 * 3, hints: ['3号种子', "6号种子"]}),
+    "5": _mkGroup({x: 255, y: 805, hints: ['第1场败者', "第2场败者"]}),
+    "6": _mkGroup({x: 255, y: 805 + 145, hints: ['第3场败者', "第4场败者"]}),
+    "7": _mkGroup({x: 655, y: 168}),
+    "8": _mkGroup({x: 655, y: 457}),
+    "9": _mkGroup({x: 655, y: 900, hints: ['第7场败者', ""]}),
+    "10": _mkGroup({x: 655, y: 755, hints: ['第8场败者', ""]}),
+    "11": _mkGroup({x: 1055, y: 312}),
+    "12": _mkGroup({x: 1055, y: 825}),
+    "13": _mkGroup({x: 1460, y: 755, hints: ['第11场败者', ""]}),
+    "14": _mkGroup({x: 1460, y: 390, hints: ['', "第13场胜者"]}),
 };
 export class BracketGroup {
     x: number;
